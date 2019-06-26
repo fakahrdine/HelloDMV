68bd3865c31a7fa96a8fa3377226086a58521349
Mohamed <fakharodine2015@gmail.com>
2019-06-26 13:34:09 -0400

    we added new project

---
+++ Day0/src/Datatype/AllDatatype.java
@@ -0,0 +1,17 @@
+package Datatype;
+
+public class AllDatatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int x=20;
+        float y=30.20f;
+        String name="score";
+        boolean z;
+        z=x<y;
+        float a=x+y;
+        System.out.println("x is greater than y "+z);
+        System.out.println("output"+name+"will be"+a);
+
+	}
+}
